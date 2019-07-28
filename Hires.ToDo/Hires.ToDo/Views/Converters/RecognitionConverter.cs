9c84563699df91eecefc6af878b1fd2d6466eed5
Matej Hires <m.hires@hotmail.cz>
2019-07-28 17:54:57 +0200

    Recognition converter

---
+++ Hires.ToDo/Hires.ToDo/Views/Converters/RecognitionConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Windows.UI.Xaml.Data;
+
+namespace Hires.ToDo.Views.Converters
+{
+    public class RecognitionConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if ((bool)value)
+                return "Stop";
+
+            return "Start";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            return true;
+        }
+    }
+}
